269d9a9b5b916a0cacdb41885426778dfc7f4de2
hghurd <hghurd@mtu.edu>
2022-09-18 17:21:55 -0400

    First commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+ray2 : stb_image_write.h main.c
+	gcc -g -Wall -std=gnu99 main.c -o ray2 -lm
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Ray {
+    double pos[3];
+    double dir[3];
+};
+
+void getRay();
+
+
+int main() {
+
+    printf("Hello, World!\n");
+    return 0;
+}
